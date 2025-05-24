a69979f2cd91a0354afdf715f2cac56984821c9d
halitklyc <halitenes.kalayci@gmail.com>
2025-05-24 13:33:54 +0300

    main.py eklendi.

---
+++ house_price_ml_aigenerated/main.py
@@ -0,0 +1,10 @@
+# Indexing => Projenizin dosyalarının ilgili AI'a aktarılması. 
+
+# Context => Sizin belirlediğiniz proje dosyalarının ilgili prompt ile aktarılması.
+
+# Back Tick => `` ``
+
+# Clean Tree => Githuba göndereceğim hiç bir değişiklik olmaması.
+
+
+# Kural 1 -> Her prompt öncesi clean tree çalış!
